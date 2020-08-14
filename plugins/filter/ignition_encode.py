59bf2c26d5315c2c3f388f380b904f606e63ceb1
Lars Kellogg-Stedman <lars@oddbit.com>
2020-08-13 21:28:51 -0400

    add testing
    this adds both a unit test and an integration test for the
    ignition_encode filter

--- plugins/filter/ignition_encode.py
+++ plugins/filter/ignition_encode.py
@@ -1,4 +1,8 @@
+from __future__ import (absolute_import, division, print_function)
+
 import base64
+
+__metaclass__ = type
 
 
 def ignition_encode(val, content_type='text/plain', charset='utf-8'):
---
+++ tests/unit/plugins/filter/test_ignition_encode.py
@@ -0,0 +1,8 @@
+from __future__ import (absolute_import, division, print_function)
+from ansible_collections.larsks.ignition.plugins.filter import ignition_encode
+
+__metaclass__ = type
+
+
+def test_ignition_encode():
+    return True
